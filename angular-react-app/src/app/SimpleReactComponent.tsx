8a6dec9374e9896643adaa929f1f35faac8466ac
Adrian Mohnacs <amohnacs@gmail.com>
2024-03-10 14:24:45 +0700

    reinit of repo to allow for git configuration to catch on

---
+++ angular-react-app/src/app/SimpleReactComponent.tsx
@@ -0,0 +1,8 @@
+// SimpleReactComponent.jsx
+import React from 'react';
+
+const SimpleReactComponent = ({ message }: { message: string }) => {
+  return <div>{message}</div>;
+};
+
+export default SimpleReactComponent;
